56cf1c602a4828b05e62988d6b9178408d1d7b7e
tunamaguro <contact@tunamaguro.dev>
2025-06-29 13:10:59 +0000

    feat: add error

--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,160 @@ pub(crate) mod query;
 use postgres::TokioPostgres;
 use query::{DbEnum, DbTypeMap, Query, ReturningRows, RsType, collect_enums};
 
+pub trait StackError: std::error::Error {
+    /// format each error stack
+    fn format_stack(&self, layer: usize, buf: &mut Vec<String>);
+    /// next error
+    fn next(&self) -> Option<&dyn StackError>;
+
+    /// last error
+    fn last(&self) -> &dyn StackError
+    where
+        Self: Sized,
+    {
+        let Some(mut result) = self.next() else {
+            return self;
+        };
+        while let Some(err) = result.next() {
+            result = err;
+        }
+        result
+    }
+}
+
+pub trait StackErrorExt: StackError {
+    fn stack_error(&self) -> Vec<String>
+    where
+        Self: Sized,
+    {
+        let mut buf = Vec::new();
+        let mut layer = 0;
+        let mut current: &dyn StackError = self;
+
+        loop {
+            current.format_stack(layer, &mut buf);
+            match current.next() {
+                Some(next) => {
+                    current = next;
+                    layer += 1;
+                }
+                None => break,
+            }
+        }
+
+        buf
+    }
+}
+
+impl<E: StackError> StackErrorExt for E {}
+
+#[derive(Debug)]
+pub enum Error {
+    Io {
+        source: std::io::Error,
+        location: &'static std::panic::Location<'static>,
+    },
+    ProstDecode {
+        source: prost::DecodeError,
+        location: &'static std::panic::Location<'static>,
+    },
+    Any {
+        source: Box<dyn std::error::Error + 'static>,
+        location: &'static std::panic::Location<'static>,
+    },
+}
+
+impl Error {
+    fn location(&self) -> &'static std::panic::Location<'static> {
+        match self {
+            Error::Io { location, .. } => location,
+            Error::ProstDecode { location, .. } => location,
+            Error::Any { location, .. } => location,
+        }
+    }
+
+    #[track_caller]
+    fn io_error(source: std::io::Error) -> Self {
+        Error::Io {
+            source,
+            location: std::panic::Location::caller(),
+        }
+    }
+
+    #[track_caller]
+    fn prost_decode(source: prost::DecodeError) -> Self {
+        Error::ProstDecode {
+            source,
+            location: std::panic::Location::caller(),
+        }
+    }
+
+    #[track_caller]
+    fn any(source: Box<dyn std::error::Error + 'static>) -> Self {
+        Error::Any {
+            source,
+            location: std::panic::Location::caller(),
+        }
+    }
+}
+
+impl From<std::io::Error> for Error {
+    #[track_caller]
+    fn from(value: std::io::Error) -> Self {
+        Error::io_error(value)
+    }
+}
+
+impl From<prost::DecodeError> for Error {
+    #[track_caller]
+    fn from(value: prost::DecodeError) -> Self {
+        Error::prost_decode(value)
+    }
+}
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Error::Io { source, .. } => source.fmt(f),
+            Error::ProstDecode { source, .. } => source.fmt(f),
+            Error::Any { source, .. } => source.fmt(f),
+        }
+    }
+}
+
+impl StackError for Error {
+    fn format_stack(&self, layer: usize, buf: &mut Vec<String>) {
+        let location = self.location();
+        let message = format!(
+            "{}:{}, at {}:{}",
+            layer,
+            self,
+            location.file(),
+            location.line()
+        );
+        buf.push(message);
+    }
+
+    fn next(&self) -> Option<&dyn StackError> {
+        None
+    }
+
+    fn last(&self) -> &dyn StackError
+    where
+        Self: Sized,
+    {
+        let Some(mut result) = self.next() else {
+            return self;
+        };
+        while let Some(err) = result.next() {
+            result = err;
+        }
+        result
+    }
+}
+
+impl std::error::Error for Error {}
+
 fn deserialize_codegen_request(data: &[u8]) -> Result<plugin::GenerateRequest, prost::DecodeError> {
     plugin::GenerateRequest::decode(data)
 }
@@ -51,12 +205,12 @@ pub(crate) trait DbCrate {
     fn call_query(row: &ReturningRows, query: &Query) -> proc_macro2::TokenStream;
 }
 
-pub fn try_main() {
+pub fn try_main() -> Result<(), Error> {
     let mut stdin = std::io::stdin().lock();
     let mut buffer = Vec::new();
-    stdin.read_to_end(&mut buffer).unwrap();
+    stdin.read_to_end(&mut buffer)?;
 
-    let request = deserialize_codegen_request(&buffer).expect("Failed to decode GenerateRequest");
+    let request = deserialize_codegen_request(&buffer)?;
 
     let mut response = plugin::GenerateResponse::default();
 
@@ -119,7 +273,7 @@ pub fn try_main() {
         #enums_tt
         #queries_tt
     };
-    let ast = syn::parse2(tt).unwrap();
+    let ast = syn::parse2(tt).map_err(|e| Error::any(e.into()))?;
     let query_file = plugin::File {
         name: "queries.rs".to_string(),
         contents: prettyplease::unparse(&ast).into(),
@@ -128,7 +282,7 @@ pub fn try_main() {
 
     let serialized_response = serialize_codegen_response(&response);
 
-    std::io::stdout()
-        .write_all(&serialized_response)
-        .expect("Failed to write response");
+    std::io::stdout().write_all(&serialized_response)?;
+
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use sqlc_gen_rust::{StackErrorExt as _, try_main};
+
 fn main() {
-    sqlc_gen_rust::try_main();
+    match try_main() {
+        Ok(()) => {}
+        Err(e) => {
+            let stack = e.stack_error().join("\n");
+            eprintln!("{}", stack)
+        }
+    }
 }
