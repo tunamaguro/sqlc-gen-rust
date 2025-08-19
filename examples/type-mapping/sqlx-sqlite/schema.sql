978d7ec5e8ad874315aa44e65a9a2a612600d50a
tunamaguro <contact@tunamaguro.dev>
2025-08-19 12:26:22 +0000

    chore: sqlite example

--- examples/type-mapping/sqlx-sqlite/schema.sql
+++ examples/type-mapping/sqlx-sqlite/schema.sql
@@ -1,13 +1,39 @@
 CREATE TABLE mapping (
-    bool_val BOOLEAN NOT NULL,
-    int_val INTEGER NOT NULL,
-    int_nullable_val INTEGER,
-    real_val REAL NOT NULL,
-    text_val TEXT NOT NULL,
-    blob_val BLOB NOT NULL,
-    datetime_val DATETIME NOT NULL,
-    date_val DATE NOT NULL,
-    time_val TIME NOT NULL,
-    json_val TEXT NOT NULL,
-    id INTEGER PRIMARY KEY AUTOINCREMENT
+  aff_integer_val         INTEGER    NOT NULL,
+  aff_real_val            REAL       NOT NULL,
+  aff_text_val            TEXT       NOT NULL,
+  aff_blob_val            BLOB       NOT NULL,
+
+  int_val                 INT        NOT NULL,
+  integer_val             INTEGER    NOT NULL,
+  tinyint_val             TINYINT    NOT NULL,
+  smallint_val            SMALLINT   NOT NULL,
+  mediumint_val           MEDIUMINT  NOT NULL,
+  bigint_val              BIGINT     NOT NULL,
+  unsigned_big_int_val    UNSIGNED BIG INT NOT NULL,
+  int2_val                INT2       NOT NULL,
+  int8_val                INT8       NOT NULL,
+
+  character20_val         CHARACTER(20) NOT NULL,
+  varchar255_val          VARCHAR(255) NOT NULL,
+  varying_char255_val     VARYING CHARACTER(255) NOT NULL,
+  nchar55_val             NCHAR(55)   NOT NULL,
+  native_char70_val       NATIVE CHARACTER(70) NOT NULL,
+  nvarchar100_val         NVARCHAR(100) NOT NULL,
+  text_val                TEXT        NOT NULL,
+  clob_val                CLOB        NOT NULL,
+
+  real_val                REAL        NOT NULL,
+  double_val              DOUBLE      NOT NULL,
+  double_precision_val    DOUBLE PRECISION NOT NULL,
+  float_val               FLOAT       NOT NULL,
+
+  numeric_val             NUMERIC     NOT NULL,
+  decimal10_5_val         DECIMAL(10,5) NOT NULL,
+  boolean_val             BOOLEAN     NOT NULL,
+  datetime_val            DATETIME    NOT NULL,
+  date_val                DATE        NOT NULL,
+  time_val                TIME        NOT NULL,
+
+  id_val                  INTEGER PRIMARY KEY AUTOINCREMENT
 );
--- examples/type-mapping/sqlx-sqlite/src/lib.rs
+++ examples/type-mapping/sqlx-sqlite/src/lib.rs
@@ -25,19 +25,39 @@ mod tests {
         let datetime_val = chrono::NaiveDateTime::from_str("2025-01-23T04:05:06").unwrap();
         let date_val = chrono::NaiveDate::from_ymd_opt(2025, 1, 23).unwrap();
         let time_val = chrono::NaiveTime::from_hms_opt(1, 23, 45).unwrap();
-        let json_val = serde_json::json!({ "type": "json" });
 
         let q = queries::InsertMapping::builder()
-            .bool_val(true)
-            .int_val(1)
-            .int_nullable_val(Some(2))
-            .real_val(3.4)
-            .text_val("text")
-            .blob_val(&blob_val)
-            .datetime_val(&datetime_val)
+            .aff_integer_val(1)
+            .aff_real_val(2.0)
+            .aff_text_val("3")
+            .aff_blob_val(&blob_val)
+            .int_val(5)
+            .integer_val(6)
+            .tinyint_val(7)
+            .smallint_val(8)
+            .mediumint_val(9)
+            .bigint_val(10)
+            .unsigned_big_int_val(11)
+            .int_2_val(12)
+            .int_8_val(13)
+            .character_20_val("14")
+            .varchar_255_val("15")
+            .varying_char_255_val("16")
+            .nchar_55_val("17")
+            .native_char_70_val("18")
+            .nvarchar_100_val("19")
+            .text_val("20")
+            .clob_val("21")
+            .real_val(22.0)
+            .double_val(23.0)
+            .double_precision_val(24.0)
+            .float_val(25.0)
+            .numeric_val(26.1)
+            .decimal_10_5_val(27.1)
+            .boolean_val(true)
             .date_val(&date_val)
             .time_val(&time_val)
-            .json_val(&json_val)
+            .datetime_val(&datetime_val)
             .build();
 
         q.execute(pool).await.unwrap();
